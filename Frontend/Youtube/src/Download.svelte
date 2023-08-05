a37e92df44936add4aafb9db386714c95b90cf7f
dhayaalan <dhayaalanjayakumar2001@gmail.com>
2023-08-05 22:43:31 +0530

    Download page Fetch method

--- Frontend/Youtube/src/Download.svelte
+++ Frontend/Youtube/src/Download.svelte
@@ -1,5 +1,19 @@
 <script>
+  import axios from 'axios';
   import Nav from './Nav.svelte';
+
+  let videoUrl = '';
+
+  const respone = async () => {
+    const res = await axios({
+      method: 'post',
+      url: 'http://localhost:5000/',
+      data: {
+        videoUrl: videoUrl,
+      },
+    });
+    console.log('videoURl', res.data);
+  };
 </script>
 
 <Nav />
@@ -10,10 +24,13 @@
       class="form-control"
       placeholder="URL Link"
       aria-label="First name"
+      bind:value={videoUrl}
     />
   </div>
 </div>
-<button type="button" class="btn btn-outline-success">Success</button>
+<button type="button" class="btn btn-outline-success" on:click={respone}
+  >Success</button
+>
 
 <style>
   div {
