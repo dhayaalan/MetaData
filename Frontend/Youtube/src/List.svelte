ef37ac1acd6286aa148794852a6a4cbdf1548b97
dhayaalan <dhayaalanjayakumar2001@gmail.com>
2023-08-07 16:27:22 +0530

    Completed the task

--- Frontend/Youtube/src/List.svelte
+++ Frontend/Youtube/src/List.svelte
@@ -1,15 +1,53 @@
 <script>
+  import axios from 'axios';
   import Nav from './Nav.svelte';
-  export let width = 20;
+  import { onMount } from 'svelte';
+  export let width = 30;
+
+  let items = [];
+
+  const fetchVideos = async () => {
+    try {
+      const response = await axios({
+        method: 'get',
+        url: 'http://localhost:5000/list',
+      });
+      console.log(response.data);
+      items = response.data;
+    } catch (error) {
+      console.error('Error fetching video data:', error);
+    }
+  };
+
+  // Function to replace slashes with underscores
+  function replaceSpacesAndEncode(filename) {
+    return encodeURIComponent(filename.replace(/ + /g, ' '));
+  }
+
+  onMount(fetchVideos);
 </script>
 
 <Nav />
-<div class="card" style="width: {width}rem;">
-  <img src="..." class="card-img-top" alt="..." />
-  <div class="card-body">
-    <p class="card-text">
-      Some quick example text to build on the card title and make up the bulk of
-      the card's content.
-    </p>
-  </div>
+<div class="div">
+  {#each items as item}
+    <!-- <div class="div"> -->
+    <div class="card" style="width: {width}%;">
+      <div class="card-body">
+        <h5 class="card-title">{item.title}</h5>
+        <p class="card-text">{item.url}</p>
+        <div class="embed-responsive embed-responsive-16by9">
+          <video class="embed-responsive-item" controls>
+            <source
+              type="video/mp4"
+              src={`http://localhost:5000/video/${replaceSpacesAndEncode(
+                item.filename
+              )}`}
+            />
+            Your browser does not support the video tag.
+            <track kind="captions" />
+          </video>
+        </div>
+      </div>
+    </div>
+  {/each}
 </div>
