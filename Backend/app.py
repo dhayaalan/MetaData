5fa8721b4655504c3013455dd01505041c231e8e
dhayaalan <dhayaalanjayakumar2001@gmail.com>
2023-08-07 11:25:35 +0530

    Video Downloaded Successfully

--- Backend/app.py
+++ Backend/app.py
@@ -1,45 +1,26 @@
-from flask import request, Flask, jsonify
+from flask import Flask, request, jsonify
 from flask_cors import CORS
-import youtube_dl
-import json
+from pytube import YouTube
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/', methods=['POST'])
+@app.route('/download', methods=['POST'])
 def download():
     data = request.get_json()
-    video_url = data.get('videoUrl')
-    print(video_url,'videoUrl')
-
+    video_link = data.get('videoUrl')
+    print(video_link)
+    
     try:
-        ydl_opts = {
-            'format': 'best',  # Get the best quality format available
-        }
-
-        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-            video_info = ydl.extract_info(video_url, download=False)
-
-        metadata = {
-            'title': video_info['title'],
-            'author': video_info['uploader'],
-            'duration': video_info['duration'],
-            'views': video_info.get('view_count', 0),
-            'likes': video_info.get('like_count', 0),
-            'dislikes': video_info.get('dislike_count', 0),
-            'description': video_info['description'],
-            'thumbnails': video_info['thumbnail'],
-            'streams': [{
-                'resolution': stream['format'],
-                'extension': stream['ext'],
-                'url': stream['url']
-            } for stream in video_info['formats'] if 'format' in stream]
-        }
-
-        # Convert the metadata to a JSON response
-        return jsonify(metadata)
+        yt = YouTube(video_link)
+        stream = yt.streams.get_highest_resolution()  # Get the highest resolution stream
+        video_title = yt.title
+        filename = f"videos/{video_title}.mp4"
+        
+        stream.download(output_path = ".",filename=filename)
+        return "Downloaded successfully"
     except Exception as e:
-        return jsonify({'error': str(e)})
+        return f"Error: {str(e)}"
 
 if __name__ == '__main__':
     app.run(debug=True)
